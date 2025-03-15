5117440eef1b9c2eeaa666ab91b8b1195a1f6729
karthik-t-g <karthikeyangopal16@gmail.com>
2025-03-15 16:47:19 +0100

    data

--- 04-Loops/loops.tf
+++ 04-Loops/loops.tf
@@ -1,3 +1,9 @@
-resources "null-resources" "test" {
+resource "null-resource" "test" {
   count=3
+}
+variable "component" {
+default = ["frontend","mongo","catalogue"]
+}
+output "component" {
+  value = length(component)
 }
--- 05-data/data.tf
+++ 05-data/data.tf
@@ -3,3 +3,6 @@ data "aws_ami" "ami" {
   name_regex  = "RHEL-9-DevOps-Practice"
   owners      = ["973714476881"]
 }
+output "ami" {
+  value =data.aws_ami.ami.name
+}
