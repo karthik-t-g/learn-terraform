b241392ac6f9e81b42b06757d302e3cdd5f1e4a0
karthik-t-g <karthikeyangopal16@gmail.com>
2025-03-15 17:12:00 +0100

    loop

--- 04-Loops/loops.tf
+++ 04-Loops/loops.tf
@@ -2,9 +2,12 @@ resource "null_resource" "test" {
   count=3
 }
 variable "component" {
-default = ["frontend","frontend","catalogue"]
+default = ["frontend","mongo","catalogue"]
 }
 output "test" {
   value= length(var.component)
 
+}
+resource "null_resource" "test1" {
+  count = length(var.component)
 }
