2ec46ed81d748bf43504e13835ad884ad69be6c1
karthik-t-g <karthikeyangopal16@gmail.com>
2025-03-15 17:22:25 +0100

    loop

--- 06-loop-1/loop-1.tf
+++ 06-loop-1/loop-1.tf
@@ -2,9 +2,6 @@ resource "null_resource" "fruits" {
   for_each = var.fruits
 
 }
-resource "null_resource" "quantity" {
-  for_each = var.fruits.apple.quantity
-}
 
 variable "fruits" {
   default = {
