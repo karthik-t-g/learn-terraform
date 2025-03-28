28939d98ee3b691cc1d5c90e03b5857cc6debdc8
karthik-t-g <karthikeyangopal16@gmail.com>
2025-03-28 17:17:30 +0100

    reoboshop

--- robo-terraform-v2/main.tf
+++ robo-terraform-v2/main.tf
@@ -1,43 +1,43 @@
 resource "aws_instance" "frontend" {
-  ami = data.aws_ami.ami
+  ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = data.aws_security_groups.sg
+  vpc_security_group_ids = data.aws_security_groups.sg.ids
   tags = {
     name="frontend"
   }
 }
 resource "aws_route53_record" "frontend"{
-  zone_id = data.aws_route53_zone.zone
+  zone_id = data.aws_route53_zone.zone.zone_id
   name = "frontend.dev.${var.domain_name}"
   type = "A"
   ttl = 300
   records = [aws_instance.frontend.private_ip]
 }
 resource "aws_instance" "mongo" {
-  ami = data.aws_ami.ami
+  ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = data.aws_security_groups.sg
+  vpc_security_group_ids = data.aws_security_groups.sg.ids
   tags = {
     name="mongo"
   }
 }
 resource "aws_route53_record" "mongo"{
-  zone_id = data.aws_route53_zone.zone
+  zone_id = data.aws_route53_zone.zone.zone_id
   name = "mongo.dev.${var.domain_name}"
   type = "A"
   ttl = 300
   records = [aws_instance.mongo.private_ip]
 }
 resource "aws_instance" "catalogue" {
-  ami = data.aws_ami.ami
+  ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = data.aws_security_groups.sg
+  vpc_security_group_ids = data.aws_security_groups.sg.ids
   tags = {
     name="catalogue"
   }
 }
 resource "aws_route53_record" "catalogue"{
-  zone_id = data.aws_route53_zone.zone
+  zone_id = data.aws_route53_zone.zone.zone_id
   name = "catalogue.dev.${var.domain_name}"
   type = "A"
   ttl = 300
